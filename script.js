da7008a3d127fd05da51c44096208ddfa024cb5f
sahumo <7523460@gmail.com>
2021-11-14 12:26:32 +0200

    удаляет последний элемент массива

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const arr = [1,2,3,6,8];
+
+arr.pop();
+
+console.log(arr);
+
+//[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
+//[ 1, 2, 3, 6 ]
+
+
