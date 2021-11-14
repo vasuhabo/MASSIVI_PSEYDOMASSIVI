ef60b50154937e6314df56b3b40b298b8e3995c3
sahumo <7523460@gmail.com>
2021-11-14 14:51:11 +0200

    делает  сначала  массив  из  данных  через  запятую и  затем  метод  join  склеивает  данные и  выводит  через  точку  с  запятой  данные

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ const  str = prompt("", "");
 
 const products =str.split(",");
 
-console.log(products);
-
-//(5) ['t', 'y', 'tjk ', 'yyk', 'uj']0: "t"1: "y"2: "tjk "3: "yyk"4:
+console.log(products.join(';'));
 
 
+//rrr;ggh;hj;yhg;ghgf;hh;
 
 
 
