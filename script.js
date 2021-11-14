d77264a4a48471ece10c0fdbd5119af04cd2b261
sahumo <7523460@gmail.com>
2021-11-14 13:00:31 +0200

    вывод  в  консоль  что  на  самом деле  лежит  в  массиве

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ arr[99] = 0;
 
 console.log(arr.length);
 
+console.log(arr);
+
 //[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
 //100
+//[ 1, 2, 3, 6, 8, <94 empty items>, 0 ]
+
+
 
