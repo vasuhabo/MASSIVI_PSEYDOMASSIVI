88d27168eab2864a595252e6087ebefbbae2e6b1
sahumo <7523460@gmail.com>
2021-11-14 12:49:01 +0200

    length   свойство   выводит количество  элементов  в  массиве

--- script.js
+++ script.js
@@ -2,16 +2,8 @@
 
 const arr = [1,2,3,6,8];
 
-
-for (let sahumo of arr) {
-  console.log(sahumo);
-}
+console.log(arr.length);
 
 // [Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
-// 1
-// 2
-// 3
-// 6
-// 8
-
+// 5
 
