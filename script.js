78824612c99da08ff5b29ca0099bb3e3cdc454fb
sahumo <7523460@gmail.com>
2021-11-14 13:12:22 +0200

    метод  forEach  как  цикл перебора  что  находиться  в  массиве  с  настройками  внутри  метода  добавления  callback function  с созданными  сразу  в  ней  аргументами  что  показывают  1 элемент  массива , 2 порядковый   номер 3 массив  полностью

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 
 const arr = [1,2,3,6,8];
 
-arr[99] = 0;
+arr.forEach(function(item,i,arr){
+console.log(`${i}: ${item} внутри массива ${arr}`);
 
-console.log(arr.length);
+})
 
-console.log(arr);
+// [Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
+// 0: 1 внутри массива 1,2,3,6,8
+// 1: 2 внутри массива 1,2,3,6,8
+// 2: 3 внутри массива 1,2,3,6,8
+// 3: 6 внутри массива 1,2,3,6,8
+// 4: 8 внутри массива 1,2,3,6,8
 
-//[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
-//100
-//[ 1, 2, 3, 6, 8, <94 empty items>, 0 ]
+
 
 
 
