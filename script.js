9c724d00f4e7ddb4bb17b7b4b412f5dd18092df1
sahumo <7523460@gmail.com>
2021-11-14 12:38:43 +0200

    перебор  массива

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 
 const arr = [1,2,3,6,8];
 
-arr.push(10);
 
-console.log(arr);
+for (let i = 0; i < arr.length;i++) {
+  console.log(arr[i]);
+}
 
 //[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
-//[ 1, 2, 3, 6, 8, 10 ]
+//1
+//2
+//3
+//6
+//8
+
 
 
