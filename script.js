df5ddb1b1d4789279b78817b6d8b823006fbe8fb
sahumo <7523460@gmail.com>
2021-11-14 12:30:57 +0200

    добавляет элемент к  массиву  в  конец

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 const arr = [1,2,3,6,8];
 
-arr.pop();
+arr.push(10);
 
 console.log(arr);
 
 //[Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
-//[ 1, 2, 3, 6 ]
+//[ 1, 2, 3, 6, 8, 10 ]
 
 
