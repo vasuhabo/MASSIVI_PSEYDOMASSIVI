ca6d5aa343b919887a0f0c84023c588ff1fbaa3b
sahumo <7523460@gmail.com>
2021-11-14 14:41:21 +0200

    split  метод   выводит   с  браузера  введенные  данные  через  запятую

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 "use strict";
 
-const arr = [1,2,3,6,8];
+const  str = prompt("", "");
 
-arr.forEach(function(item,i,arr){
-console.log(`${i}: ${item} внутри массива ${arr}`);
+const products =str.split(",");
 
-})
+console.log(products);
 
-// [Running] node "w:\КУРС ДЖАВА СКРИПТ\1\18 MASSIVI_PSEYDOMASSIVI\tempCodeRunnerFile.js"
-// 0: 1 внутри массива 1,2,3,6,8
-// 1: 2 внутри массива 1,2,3,6,8
-// 2: 3 внутри массива 1,2,3,6,8
-// 3: 6 внутри массива 1,2,3,6,8
-// 4: 8 внутри массива 1,2,3,6,8
+//(5) ['t', 'y', 'tjk ', 'yyk', 'uj']0: "t"1: "y"2: "tjk "3: "yyk"4:
+
 
 
 
